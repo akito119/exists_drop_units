5ddacce76e8ba8c6aa34b0aeddf07c670e2ff249
yuki <akito.nishi04@gmail.com>
2026-01-30 13:05:26 +0900

    DPを実装

--- src/components/judge.tsx
+++ src/components/judge.tsx
@@ -4,39 +4,88 @@ type Props = {
   values: number[];
   value: number;
 };
-
 function allSums(a: number[]): number[] {
   const result: number[] = [];
 
-  function dfs(index: number, currentSum: number) {
+  function dfs(
+    index: number,
+    currentSum: number,
+    usedZero: boolean
+  ) {
     if (index === a.length) {
-      result.push(currentSum);
+      if (usedZero) {
+        result.push(currentSum);
+      }
       return;
     }
+
     for (let k = 0; k <= 4; k++) {
-      dfs(index + 1, currentSum + k * a[index]);
+      dfs(
+        index + 1,
+        currentSum + k * a[index],
+        usedZero || k === 0
+      );
     }
   }
 
-  dfs(0, 0);
+  dfs(0, 0, false);
   return result;
+}
+function DPjudge (values:number[],value:number){
+  let total:number =0;
+  for(let i = 0;i<values.length;i++){
+    total+=values[i];
+  }
+  const GPA:number =Math.round(total*value);
+  const DP:number[]=new Array(GPA+2).fill(0);
+  DP[0]=1;
+  for(let i = 0;i<values.length;i++){
+    let update:number[]=[];
+    for(let k=1;k<5;k++){
+      for(let j=0;j<GPA+2;j++){
+      if(DP[j]===1&&k===1){
+        update.push(j);
+      }
+      if(j>=k*values[i]){
+        if(DP[j-k*values[i]]>0&&DP[j]===0){
+          update.push(j);
+        }
+      }
+    }
+    }
+    for(let j=0;j<update.length;j++){
+      DP[update[j]]++;
+    }
+  }
+  if(DP[GPA]>1||DP[GPA+1]>1||DP[GPA-1]>1){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 const Judge: React.FC<Props> = ({ values, value }) => {
   const [flag, setFlag] = useState<boolean | null>(null);
-
   const handleJudge = () => {
-    const total = values.length;
+    let total = 0;
+    for(let i=0;i<values.length;i++){
+        total+=values[i];
+    }
     const sums = allSums(values);
 
+    const gpa100 = Math.round(value * 100);
+    const target = gpa100 * total;
     let result = false;
     for (let i = 0; i < sums.length; i++) {
-      if (Math.abs(value * total - sums[i]) < 0.1) {
+        const sum100 = sums[i] * 100;
+        console.log(sums[i]);
+      if (Math.abs(target - sum100) < 1) {
         result = true;
         break;
       }
     }
-
+    result = DPjudge(values,value);
     setFlag(result);
   };
 
